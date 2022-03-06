6511748f7fdf42406d0173bbdd759a8a240ef8cf
Philip Turner <71743241+philipturner@users.noreply.github.com>
2022-03-06 17:57:46 -0500

    Update install_swift.sh

--- install_swift.sh
+++ install_swift.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 
 if [[ ! -d /opt/swift ]]; then
   mkdir /opt/swift
@@ -21,15 +21,20 @@ fi
 
 # Determine whether to reuse cached files
 
-using_cached_swift=true
-
 if [[ -e "swift-version.txt" ]]; then
   old_version = `cat "swift-version.txt"`
   
-  if [[ version != old_version ]]; then
+  if [[ version == old_version ]]; then
+    using_cached_swift=true
+  elif [[ -e "toolchain-$version" ]]; then
+    mv -r "toolchain-$version" toolchain
+    using_cached_swift=true
+  else
     mv -r toolchain "toolchain-$old_version"
     using_cached_swift=false
   fi
+else
+  using_cached_swift=false
 fi
 
 if [[ using_cached_swift == false && -e toolchain ]]; then
@@ -41,6 +46,7 @@ fi
 
 # instead, say "using cached download: ---" whenever possible
 echo "Downloading Swift $version"
+echo $using_cached_swift
 
 # write to swift-version.txt immediately AFTER finish downloading
 
