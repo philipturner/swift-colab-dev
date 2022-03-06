46499fb32bef6ff5304d1acbaea8f7eac9f668d9
Philip Turner <71743241+philipturner@users.noreply.github.com>
2022-03-06 17:16:56 -0500

    Update install_swift.sh

--- install_swift.sh
+++ install_swift.sh
@@ -10,10 +10,10 @@ is_dev=false
 swift_version="-1"
 
 if [[ $# == 2 && "$1" == "-dev" ]]; then
-  is_dev = true
-  swift_version = "dev $2"
+  is_dev=true
+  swift_version="dev $2"
 elif [[ $# == 1 ]]; then
-  swift_version = "$1"
+  swift_version="$1"
 else
   echo "Usage: bash install_swift.sh [-dev] VERSION"
 fi
