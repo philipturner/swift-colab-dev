e0e98a912b770a8066263a823181a262dac62bc5
Philip Turner <71743241+philipturner@users.noreply.github.com>
2022-03-06 17:17:38 -0500

    Update install_swift.sh

--- install_swift.sh
+++ install_swift.sh
@@ -18,5 +18,11 @@ else
   echo "Usage: bash install_swift.sh [-dev] VERSION"
 fi
 
+if [[ $is_dev == false ]]; then
+  echo "hello world 1"
+else
+  echo "hello world 2"
+fi
+
 echo $swift_version
 echo $is_dev
