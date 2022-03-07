75d6bf7669a1c840ba5feb0c695f640321bbb017
Philip Turner <71743241+philipturner@users.noreply.github.com>
2022-03-07 09:12:53 -0500

    Update lldb_process.cpp

--- Sources/lldb-process/lldb_process.cpp
+++ Sources/lldb-process/lldb_process.cpp
@@ -5,15 +5,15 @@ extern "C" {
 
 int func1(char *input)
 {
-//   lldb::SBDebugger::Initialize();
+  lldb::SBDebugger::Initialize();
   auto debugger = lldb::SBDebugger::Create();
   
-//   auto expr_opts = lldb::SBExpressionOptions();
-//   auto swift_language = lldb::SBLanguageRuntime::GetLanguageTypeFromString("swift");
-//   expr_opts.SetLanguage(swift_language);
-//   expr_opts.SetREPLMode(true);
-//   expr_opts.SetUnwindOnError(false);
-//   expr_opts.SetGenerateDebugInfo(true);
+  auto expr_opts = lldb::SBExpressionOptions();
+  auto swift_language = lldb::SBLanguageRuntime::GetLanguageTypeFromString("swift");
+  expr_opts.SetLanguage(swift_language);
+  expr_opts.SetREPLMode(true);
+  expr_opts.SetUnwindOnError(false);
+  expr_opts.SetGenerateDebugInfo(true);
   
   puts(input);
   return 5;
