7442dda645adcecc2251a088042217d0623b99a5
Philip Turner <71743241+philipturner@users.noreply.github.com>
2022-03-07 07:27:45 -0500

    Update install_swift.sh

--- install_swift.sh
+++ install_swift.sh
@@ -131,7 +131,7 @@ if [[ ! -e "progress/compiled-lldb-bindings" ]]; then
   
   lldb_link_path="/opt/swift/toolchain/usr/lib/liblldb.so"
   lldb_link_target="$(readlink $lldb_link_path)"
-#   patchelf --replace-needed $lldb_link_target $lldb_link_path liblldb_process.so
+  patchelf --replace-needed $lldb_link_target $lldb_link_path liblldb_process.so
 
   # ensure two-command link check isn't what's breaking this
   lldb_process_link=/opt/swift/lib/liblldb_process.so
