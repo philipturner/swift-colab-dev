bd3a940579363831d635c66b93e43382df2dbe28
Philip Turner <71743241+philipturner@users.noreply.github.com>
2022-03-07 07:28:40 -0500

    Update validate.swift

--- Sources/lldb-process/validate.swift
+++ Sources/lldb-process/validate.swift
@@ -3,13 +3,13 @@ import Foundation
 let lldb_process = dlopen("/opt/swift/lib/liblldb_process.so", RTLD_LAZY | RTLD_GLOBAL)
 print(lldb_process)
 
-// func loadSymbol<T>(name: String) -> T {
-//   let address = dlsym(lldb_process, name)
-//   print(address)
-//   return unsafeBitCast(address, to: T.self)
-// }
+func loadSymbol<T>(name: String) -> T {
+  let address = dlsym(lldb_process, name)
+  print(address)
+  return unsafeBitCast(address, to: T.self)
+}
 
-// let func1: @convention(c) (UnsafePointer<CChar>) -> Int32 =
-//   loadSymbol(name: "func1")
+let func1: @convention(c) (UnsafePointer<CChar>) -> Int32 =
+  loadSymbol(name: "func1")
 
 // print(func1("success output"))
