7ab5bf65fa5e7a24b636bbc26d77e5572053ae94
Philip Turner <71743241+philipturner@users.noreply.github.com>
2022-03-07 13:03:56 -0500

    Update validate.swift

--- Sources/lldb-process/validate.swift
+++ Sources/lldb-process/validate.swift
@@ -1,14 +1,9 @@
 import Foundation
-let fileName = "validate.swift"
-print("=== begin '\(fileName)' ===")
-print()
 
 // For an unknown reason, only `liblldb.so` crashes when using a symbolic link at `/opt/swift/lib`
-print("Debug checkpoint 1")
 let lldb = dlopen("/opt/swift/toolchain/usr/lib/liblldb.so", RTLD_LAZY | RTLD_GLOBAL)
 print("Should not be 'nil':", lldb as Any)
 
-print("Debug checkpoint 2")
 let lldb_process = dlopen("/opt/swift/lib/liblldb_process.so", RTLD_LAZY | RTLD_GLOBAL)
 print("Should not be 'nil':", lldb_process as Any)
 
@@ -20,7 +15,6 @@ func loadSymbol<T>(name: String) -> T {
 
 let validation_test: @convention(c) (UnsafePointer<CChar>) -> Int32 =
   loadSymbol(name: "validation_test")
-print("Debug checkpoint 3")
 
 // For an unknown reason, it prints an error saying:
 //   ModuleNotFoundError: No module named 'lldb'
@@ -28,7 +22,3 @@ print("Debug checkpoint 3")
 // Regardless, the error seems to cause no harm.
 print("Should see 'success output' logged. Ignore any 'ModuleNotFoundError'.")
 print(validation_test("success output"))
-print("Debug checkpoint 4")
-
-print()
-print("=== end '\(fileName)' ===")
