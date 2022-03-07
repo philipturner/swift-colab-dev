4a6c848e2d1cdd43492083fa7026ed6a6127942a
Philip Turner <71743241+philipturner@users.noreply.github.com>
2022-03-06 22:27:24 -0500

    Update validate.swift

--- Sources/lldb-process/validate.swift
+++ Sources/lldb-process/validate.swift
@@ -4,7 +4,7 @@ print("hello world")
 
 // print(FileManager.default.contents(atPath: "/opt/swift/lib/libPythonKit.so") as Any)
 
-let lldb_process = dlopen("/opt/swift/toolchain/usr/lib/lidlldb.so", RTLD_LAZY | RTLD_GLOBAL)
+let lldb_process = dlopen("/opt/swift/toolchain/usr/lib/lidlldb.so.13.0.0git", RTLD_LAZY | RTLD_GLOBAL)
 // let lldb_process = dlopen("/opt/swift/packages/PythonKit/.build/release/libPythonKit.so", RTLD_LAZY | RTLD_GLOBAL)
 // print(String(cString: dlerror()))
 print(lldb_process)
