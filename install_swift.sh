6b917cbf1e78e6032a87a36ec21077a91a2b5345
Philip Turner <71743241+philipturner@users.noreply.github.com>
2022-03-07 07:37:26 -0500

    Update install_swift.sh

--- install_swift.sh
+++ install_swift.sh
@@ -151,6 +151,11 @@ if [[ ! -e "progress/compiled-lldb-bindings" ]]; then
   echo $(cat /opt/swift/swift-colab/Sources/lldb-process/build/liblldb_process.so)
   ls /opt/swift/lib
   
+  echo "path is"
+  echo $LD_LIBRARY_PATH
+  echo "end path is"
+  
+  export LD_LIBRARY_PATH="/opt/swift/toolchain/usr/lib:$LD_LIBRARY_PATH"
   swift validate.swift
   
   cd /opt/swift
