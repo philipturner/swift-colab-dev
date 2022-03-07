549032d3158cb53d1bf2013a2c5a3a11e215c131
Philip Turner <71743241+philipturner@users.noreply.github.com>
2022-03-07 14:32:52 -0500

    Update CreateKernel.swift

--- Sources/JupyterKernel/CreateKernel.swift
+++ Sources/JupyterKernel/CreateKernel.swift
@@ -1,7 +1,18 @@
 import Foundation
 import PythonKit
 
+fileprivate let signal = Python.import("signal")
+fileprivate let ipykernel = Python.import("ipykernel")
+
 @_cdecl("JupyterKernel_createSwiftKernel")
 public func JupyterKernel_createSwiftKernel() {
+  // TODO: remove this notice
   print("=== Creating Swift kernel ===")
+  
+  // Jupyter sends us SIGINT when the user requests execution interruption.
+  // Here, we block all threads from receiving the SIGINT, so that we can
+  // handle it in a specific handler thread.
+  signal.pthread_sigmask(signal.SIG_BLOCK, [signal.SIGINT])
+  
+  // TODO: launch kernel
 }
