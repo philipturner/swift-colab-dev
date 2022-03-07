5fd601aa27665af03d5faf31aae7b84caffe106b
Philip Turner <71743241+philipturner@users.noreply.github.com>
2022-03-07 14:22:15 -0500

    Update RegisterKernel.swift

--- Sources/JupyterKernel/RegisterKernel.swift
+++ Sources/JupyterKernel/RegisterKernel.swift
@@ -4,4 +4,11 @@ import PythonKit
 @_cdecl("JupyterKernel_registerSwiftKernel")
 public func JupyterKernel_registerSwiftKernel() {
   print("=== Registering Swift Jupyter kernel ===")
+  
+  let pythonScript = """
+  from ctypes import *
+  from wurlitzer import sys_pipes
+  
+  with sys_pipes():
+  """
 }
