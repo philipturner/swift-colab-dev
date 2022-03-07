bdf0075a1d9e72161eb8ddd3083d89a77b967959
Philip Turner <71743241+philipturner@users.noreply.github.com>
2022-03-07 09:17:08 -0500

    Update validate.swift

--- Sources/lldb-process/validate.swift
+++ Sources/lldb-process/validate.swift
@@ -1,16 +1,18 @@
 import Foundation
-print("hello world 0")
-// For an unknown reason, only `liblldb.so` crashes when using a symbolic link at `/opt/swift/lib`
-let lldb = dlopen("/opt/swift/toolchain/usr/lib/liblldb.so", RTLD_LAZY | RTLD_GLOBAL)
-print(lldb as Any)
+let fileName = "validate.swift"
 
-print("hello world 1")
+// For an unknown reason, only `liblldb.so` crashes when using a symbolic link at `/opt/swift/lib`
+print("\(fileName): Debug checkpoint 1")
+let lldb = dlopen("/opt/swift/toolchain/usr/lib/liblldb.so", RTLD_LAZY | RTLD_GLOBAL)
+print("Should not be `nil`:", lldb as Any)
+
+print("\(fileName): Debug checkpoint 2")
 let lldb_process = dlopen("/opt/swift/lib/liblldb_process.so", RTLD_LAZY | RTLD_GLOBAL)
-print(lldb_process as Any)
+print("Should not be `nil`:", lldb_process as Any)
 
 func loadSymbol<T>(name: String) -> T {
   let address = dlsym(lldb_process, name)
-  print(address as Any)
+  print("Should not be `nil`:", address as Any)
   return unsafeBitCast(address, to: T.self)
 }
 
@@ -21,5 +23,6 @@ let func1: @convention(c) (UnsafePointer<CChar>) -> Int32 =
 //   ModuleNotFoundError: No module named 'lldb'
 // This error only happens on dev toolchains, maybe because the headers are for LLDB 10? 
 // Regardless, the error seems to cause no harm.
+print("Should see `success output` logged")
 print(func1("success output"))
-print("hello world 2")
+print("\(fileName): Debug checkpoint 3")
