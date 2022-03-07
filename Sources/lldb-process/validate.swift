1bd977a66e7bb55de50e804d7ebb4405969df25a
Philip Turner <71743241+philipturner@users.noreply.github.com>
2022-03-06 22:18:25 -0500

    Update validate.swift

--- Sources/lldb-process/validate.swift
+++ Sources/lldb-process/validate.swift
@@ -5,7 +5,7 @@ print("hello world")
 // print(FileManager.default.contents(atPath: "/opt/swift/lib/libPythonKit.so") as Any)
 
 let lldb_process = dlopen("/opt/swift/lib/libPythonKit.so", RTLD_LAZY | RTLD_GLOBAL)
-print(dlerror())
+print(String(cString: dlerror()))
 print(lldb_process)
 
 // func loadSymbol<T>(name: String) -> T {
