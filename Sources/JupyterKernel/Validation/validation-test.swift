1235eef625ddf410704903f2aeb400f479dd24fb
Philip Turner <71743241+philipturner@users.noreply.github.com>
2022-03-07 13:11:09 -0500

    Update validation-test.swift

--- Sources/JupyterKernel/Validation/validation-test.swift
+++ Sources/JupyterKernel/Validation/validation-test.swift
@@ -3,7 +3,7 @@ import PythonKit
 
 // does this need to be public?
 @_cdecl("validation_test")
-func validation_test() {
+public func validation_test() {
   print("Should be '42':", meaningOfLife)
   print("Should be 'None':", Python.None)
 }
