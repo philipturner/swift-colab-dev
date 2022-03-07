09fd691ac1161ba20503f002c5939e5fe81b8ff0
Philip Turner <71743241+philipturner@users.noreply.github.com>
2022-03-07 07:24:52 -0500

    Update install_swift.sh

--- install_swift.sh
+++ install_swift.sh
@@ -134,11 +134,16 @@ if [[ ! -e "progress/compiled-lldb-bindings" ]]; then
 #   patchelf --replace-needed $lldb_link_target $lldb_link_path liblldb_process.so
 
   # ensure two-command link check isn't what's breaking this
-  if [[ -e "/opt/swift/lib/llblldb_process.so" ]]; then
-    rm "/opt/swift/lib/llblldb_process.so"
+  lldb_process_link=/opt/swift/lib/liblldb_process.so
+  if [[ ! -L $lldb_process_link ]]; then
+    ln -s "$(pwd)/liblldb_process.so" $lldb_process_link
   fi
-#   cp ./liblldb_process.so /opt/swift/lib/liblldb_process.so
-  ln -s "$(pwd)/liblldb_process.so" /opt/swift/lib/liblldb_process.so
+  
+#   if [[ -e "/opt/swift/lib/llblldb_process.so" ]]; then
+#     rm "/opt/swift/lib/llblldb_process.so"
+#   fi
+# #   cp ./liblldb_process.so /opt/swift/lib/liblldb_process.so
+#   ln -s "$(pwd)/liblldb_process.so" /opt/swift/lib/liblldb_process.so
   
   cd ../
   cat "/opt/swift/lib/llblldb_process.so"
