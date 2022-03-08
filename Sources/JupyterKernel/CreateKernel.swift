477aca7083c55f06b5ef2b8008308139e0c1acd8
Philip Turner <71743241+philipturner@users.noreply.github.com>
2022-03-08 12:07:34 -0500

    Update CreateKernel.swift

--- Sources/JupyterKernel/CreateKernel.swift
+++ Sources/JupyterKernel/CreateKernel.swift
@@ -62,20 +62,14 @@ fileprivate func activatePythonKernel() {
       app.launch_new_instance()
   */
   
-  let sys = Python.import("sys")
-  
-  if sys.path[0] == PythonObject("") {
-    sys.path[0] = Python.None
-  }
-  
   PyRun_SimpleString("""
   import sys
   
   if __name__ == '__main__':
       # Remove the CWD from sys.path while we load stuff.
       # This is added back by InteractiveShellApp.init_path()
-      # if sys.path[0] == '':
-      #     del sys.path[0]
+      if sys.path[0] == '':
+          del sys.path[0]
   
       from ipykernel import kernelapp as app
       app.launch_new_instance()          
