567077661b89513984a6fe94ab49891969cc5a29
Philip Turner <71743241+philipturner@users.noreply.github.com>
2022-03-08 11:30:49 -0500

    Update CreateKernel.swift

--- Sources/JupyterKernel/CreateKernel.swift
+++ Sources/JupyterKernel/CreateKernel.swift
@@ -5,27 +5,28 @@ fileprivate let ipykernel = Python.import("ipykernel")
 
 @_cdecl("JupyterKernel_createSwiftKernel")
 public func JupyterKernel_createSwiftKernel() {
+  activateSwiftKernel()
+  
+  activatePythonKernel()
+}
+
+fileprivate func activateSwiftKernel() {
   // TODO: remove this notice
   print("=== Creating Swift kernel ===")
   
   // Jupyter sends us SIGINT when the user requests execution interruption.
   // Here, we block all threads from receiving the SIGINT, so that we can
   // handle it in a specific handler thread.
-//   signal.pthread_sigmask(signal.SIG_BLOCK, [signal.SIGINT])
+  signal.pthread_sigmask(signal.SIG_BLOCK, [signal.SIGINT])
   
   // TODO: launch kernel
-  
-  // Until there is a built-in alternative, switch back into Python mode on the next
+}
+
+// Until there is a built-in alternative, switch back into Python mode on the next
   // runtime restart. This makes debugging a lot easier and decreases the chance my
   // main account will be kicked off of Colab for excessive restarts/downloads.
-  activatePythonKernel()
-}
-
 fileprivate func activatePythonKernel() {
-  
-}
-
-/*
+  /*
 """Entry point for launching an IPython kernel.
 
 This is separate from the ipykernel package so we can avoid doing imports until
@@ -47,3 +48,6 @@ if __name__ == '__main__':
 
 
 */
+
+}
+
