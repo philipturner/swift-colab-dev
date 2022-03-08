d8257ace0767649b4521cad820eaf5f45705ba9a
Philip Turner <71743241+philipturner@users.noreply.github.com>
2022-03-08 11:23:36 -0500

    Update CreateKernel.swift

--- Sources/JupyterKernel/CreateKernel.swift
+++ Sources/JupyterKernel/CreateKernel.swift
@@ -25,3 +25,24 @@ public func JupyterKernel_createSwiftKernel() {
   let pythonKernelPath = "/opt/swift/packages/JupyterKernel/python_kernel.py"
   try! FileManager.default.copyItem(atPath: pythonKernelPath, toPath: activeKernelPath)
 }
+
+/*
+"""Entry point for launching an IPython kernel.
+
+This is separate from the ipykernel package so we can avoid doing imports until
+after removing the cwd from sys.path.
+"""
+
+import sys
+
+if __name__ == '__main__':
+    # Remove the CWD from sys.path while we load stuff.
+    # This is added back by InteractiveShellApp.init_path()
+    if sys.path[0] == '':
+        del sys.path[0]
+
+    from ipykernel import kernelapp as app
+    app.launch_new_instance()
+
+
+*/
