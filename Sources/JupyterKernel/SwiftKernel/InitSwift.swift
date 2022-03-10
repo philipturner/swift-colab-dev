2d7348393c8b6209b7ccaec6056e03725991cbef
Philip Turner <71743241+philipturner@users.noreply.github.com>
2022-03-10 16:59:59 -0500

    Update InitSwift.swift

--- Sources/JupyterKernel/SwiftKernel/InitSwift.swift
+++ Sources/JupyterKernel/SwiftKernel/InitSwift.swift
@@ -6,9 +6,10 @@ fileprivate struct CEnvironment {
   init() {
     var environmentArray: [String] = []
     for (key, value) in ProcessInfo.processInfo.environment {
-      
+      environmentArray.append("\(key)=\(value)")
     }
-    
+    typealias EnvPointerType = UnsafeMutablePointer<UnsafePointer<CChar>>
+    let envPointer = EnvPointer.allocate(capacity
     envp = OpaquePointer(malloc(8))
   }
 }
