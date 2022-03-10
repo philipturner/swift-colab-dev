0ab7820699f3f0b630316e368d297233f05c1e86
Philip Turner <71743241+philipturner@users.noreply.github.com>
2022-03-10 15:56:14 -0500

    Update KernelContext.swift

--- Sources/JupyterKernel/SwiftKernel/KernelContext.swift
+++ Sources/JupyterKernel/SwiftKernel/KernelContext.swift
@@ -3,7 +3,7 @@ import Foundation
 struct KernelContext {
   var debuggerInitialized = false
   
-  let validation_test: @convention(c) (UnsafePonter<CChar>) -> Int32 = 
+  let validation_test: @convention(c) (UnsafePointer<CChar>) -> Int32 = 
     LLDBProcessLibrary.loadSymbol(name: "validation_test")
 }
 
@@ -11,7 +11,7 @@ fileprivate struct LLDBProcessLibrary {
   static var lldb_process: UnsafeMutableRawPointer = {
     _ = dlopen("/opt/swift/toolchain/usr/lib/liblldb.so", RTLD_LAZY | RTLD_GLOBAL)!
     return dlopen("/opt/swift/lib/liblldb_process.so", RTLD_LAZY | RTLD_GLOBAL)!
-  }
+  }()
   
   static func loadSymbol<T>(name: String) -> T {
     let address = dlsym(lldb_process, name)
