5362bc2aa94ba2f573bdac4853890fc2d9134190
Philip Turner <71743241+philipturner@users.noreply.github.com>
2022-03-10 17:27:10 -0500

    Update InitSwift.swift

--- Sources/JupyterKernel/SwiftKernel/InitSwift.swift
+++ Sources/JupyterKernel/SwiftKernel/InitSwift.swift
@@ -21,26 +21,10 @@ fileprivate struct CEnvironment {
     }
     envp = OpaquePointer(envPointer)
   }
-  
-  func validate(environment: [String: String]) {
-    var envArray: [String] = []
-    for (key, value) in environment {
-      envArray.append("\(key)=\(value)")
-    }
-    typealias ConstEnvPointerType = UnsafeMutablePointer<UnsafePointer<CChar>>
-    let envPointer = ConstEnvPointerType(envp)
-    var envArray2: [String] = []
-    for i in 0..<envArray.count {
-      envArray2.append(String(cString: envPointer[i]))
-    }
-    precondition(envArray == envArray2, "Did not match: \(envArray) and \(envArray2)")
-    print("Did match: \(envArray) and \(envArray2)")
-  }
 }
 
 func initSwift() throws {
   KernelContext.initialize_debugger(nil)
   let environment = ProcessInfo.processInfo.environment
   let cEnvironment = CEnvironment(environment: environment)
-  cEnvironment.validate(environment: environment)
 }
