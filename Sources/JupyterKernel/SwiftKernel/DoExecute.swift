1e7ba77b687aec9dd089db9c0f45616ca723cb78
Philip Turner <71743241+philipturner@users.noreply.github.com>
2022-03-10 16:01:43 -0500

    Update DoExecute.swift

--- Sources/JupyterKernel/SwiftKernel/DoExecute.swift
+++ Sources/JupyterKernel/SwiftKernel/DoExecute.swift
@@ -1,5 +1,9 @@
 import Foundation
 
 func doExecute(kernel: PythonObject, code: String) throws -> PythonObject? {
+  if !KernelContext.debuggerInitialized {
+    KernelContext.initialize_debugger()
+    KernelContext.debuggerInitialized = true
+  }
   return nil
 }
