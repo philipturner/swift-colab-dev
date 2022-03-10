25c8276e82d355c6d8a06d7195a09a6560559dcc
Philip Turner <71743241+philipturner@users.noreply.github.com>
2022-03-10 16:54:51 -0500

    Update InitSwift.swift

--- Sources/JupyterKernel/SwiftKernel/InitSwift.swift
+++ Sources/JupyterKernel/SwiftKernel/InitSwift.swift
@@ -1,5 +1,18 @@
 import Foundation
 
+fileprivate struct CEnvironment {
+  var envp: OpaquePointer
+  
+  init() {
+    var environmentArray: [String] = []
+    for (key, value) in ProcessInfo.processInfo.environment {
+      
+    }
+    
+    envp = OpaquePointer(malloc(8))
+  }
+}
+
 func initSwift() throws {
   KernelContext.initialize_debugger(nil)
 }
