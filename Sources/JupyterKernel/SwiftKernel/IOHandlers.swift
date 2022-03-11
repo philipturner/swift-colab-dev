be176b6293612ec7f0d8c018f2ea1fa2a42e058c
Philip Turner <71743241+philipturner@users.noreply.github.com>
2022-03-10 20:57:47 -0500

    Update IOHandlers.swift

--- Sources/JupyterKernel/SwiftKernel/IOHandlers.swift
+++ Sources/JupyterKernel/SwiftKernel/IOHandlers.swift
@@ -1,14 +1,25 @@
 import Foundation
 
+fileprivate let signal = Python.import("signal")
 fileprivate let threading = Python.import("threading")
 
 let SIGINTHandler = PythonClass(
   "SIGINTHandler",
   superclasses: [threading.Thread],
   members: [
-    "__init__": PythonInstanceMethod { (`self`: PythonObject) in
-      self.daemon = true
-    }              
+    "__init__": 
+    PythonInstanceMethod { (`self`: PythonObject) in
+      `self`.daemon = true
+      return Python.None
+    },
+    
+    "run": PythonInstanceMethod { (`self`: PythonObject) in
+      while true {
+        signal.sigwait([signal.SIGINT])
+        KernelContext.kernel.process.SendAsyncInterrupt()
+      }
+      return Python.None
+    }
   ]
 ).pythonObject
 
