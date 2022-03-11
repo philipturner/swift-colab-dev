45d4659100f01c03bedaf8ef3a184acae49516e6
Philip Turner <71743241+philipturner@users.noreply.github.com>
2022-03-10 19:53:22 -0500

    Update PreprocessAndExecute.swift

--- Sources/JupyterKernel/SwiftKernel/PreprocessAndExecute.swift
+++ Sources/JupyterKernel/SwiftKernel/PreprocessAndExecute.swift
@@ -1,7 +1,16 @@
 import Foundation
 
+func preprocessAndExecute(code: String) throws -> ExecutionResult {
+  do {
+    let preprocessed = try preprocess(code: code)
+    return execute(code: preprocessed)
+  } catch let e as PreprocessorException {
+    return PreprocessorError(exception: e)
+  }
+}
+
 // TODO: test that this function works
-func execute(code: String) throws -> ExecutionResult {
+func execute(code: String) -> ExecutionResult {
   var descriptionPtr: UnsafeMutablePointer<CChar>?
   let err = KernelContext.execute(code, &descriptionPtr)
   
