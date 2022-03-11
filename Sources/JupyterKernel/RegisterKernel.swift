31732673c540a55b534fb314086c7ded73ec9db7
Philip Turner <71743241+philipturner@users.noreply.github.com>
2022-03-10 20:23:47 -0500

    Update RegisterKernel.swift

--- Sources/JupyterKernel/RegisterKernel.swift
+++ Sources/JupyterKernel/RegisterKernel.swift
@@ -34,11 +34,11 @@ public func JupyterKernel_registerSwiftKernel() {
     "display_name": "Swift",
     "language": "swift",
     "env": {
-      "PYTHONPATH": "\(swift_toolchain)/usr/lib/python3/dist-packages",
-      "LD_LIBRARY_PATH": "\(swift_toolchain)/usr/lib/swift/linux",
-      "REPL_SWIFT_PATH": "\(swift_toolchain)/usr/bin/repl_swift",
-      "SWIFT_BUILD_PATH": "\(swift_toolchain)/usr/bin/swift-build",
-      "SWIFT_PACKAGE_PATH": "\(swift_toolchain)/usr/bin/swift-package"
+      "PYTHONPATH": "\(swiftToolchain)/usr/lib/python3/dist-packages",
+      "LD_LIBRARY_PATH": "\(swiftToolchain)/usr/lib/swift/linux",
+      "REPL_SWIFT_PATH": "\(swiftToolchain)/usr/bin/repl_swift",
+      "SWIFT_BUILD_PATH": "\(swiftToolchain)/usr/bin/swift-build",
+      "SWIFT_PACKAGE_PATH": "\(swiftToolchain)/usr/bin/swift-package"
     }
   }
   """
