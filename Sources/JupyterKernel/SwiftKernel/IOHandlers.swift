a749656c7725fc69803fd998399d52fa1db885de
Philip Turner <71743241+philipturner@users.noreply.github.com>
2022-03-10 20:49:10 -0500

    Update IOHandlers.swift

--- Sources/JupyterKernel/SwiftKernel/IOHandlers.swift
+++ Sources/JupyterKernel/SwiftKernel/IOHandlers.swift
@@ -1,1 +1,20 @@
 import Foundation
+
+fileprivate let threading = Python.import("threading")
+
+let SIGINTHandler = PythonClass(
+  "SIGINTHandler",
+  superclasses: [threading.Thread],
+  members: [
+    
+  ]
+}.pythonObject
+
+
+let StdoutHandler = PythonClass(
+  "StdoutHandler",
+  superclasses: [threading.Thread],
+  members: [
+    
+  ]
+}.pythonObject
