015c1f9103c6722521bb3293f7f5d56228dbc396
Philip Turner <71743241+philipturner@users.noreply.github.com>
2022-03-10 20:05:04 -0500

    Update InitSwift.swift

--- Sources/JupyterKernel/SwiftKernel/InitSwift.swift
+++ Sources/JupyterKernel/SwiftKernel/InitSwift.swift
@@ -25,6 +25,7 @@ fileprivate struct CEnvironment {
 
 func initSwift() throws {
   initReplProcess()
+  try initBitWidth()
 }
 
 fileprivate func initReplProcess() {
@@ -36,5 +37,10 @@ fileprivate func initReplProcess() {
 }
 
 fileprivate func initBitWidth() throws {
-
+  let result = execute(code: "Int.bitWidth")
+  guard let result = result as? SuccessWithValue else {
+    throw Exception("Expected value from Int.bitWidth, but got: \(result.description)")
+  }
+  precondition(result.description.contains("64"), 
+    "Int.bitWidth returned \(result.description) when '64' was expected.")
 }
