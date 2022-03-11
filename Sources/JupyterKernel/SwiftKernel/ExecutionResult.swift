ac35189a6d64cefcdec60e50c101489c8dbe18d3
Philip Turner <71743241+philipturner@users.noreply.github.com>
2022-03-10 19:25:58 -0500

    Create ExecutionResult.swift

---
+++ Sources/JupyterKernel/SwiftKernel/ExecutionResult.swift
@@ -0,0 +1,46 @@
+import Foundation
+
+/// Protocol for the result of executing code.
+protocol ExecutionResult: CustomStringConvertible { }
+
+/// Protocol for the result of successfully executing code.
+protocol ExecutionResultSuccess: ExecutionResult { }
+
+/// Protocol for the result of unsuccessfully executing code.
+protocol ExecutionResultError: ExecutionResult { 
+    var description: String { get }
+}
+
+/// The code executed successfully, and did not produce a value.
+struct SuccessWithoutValue: ExecutionResultSuccess {
+    var description: String { "" }
+}
+
+/// The code executed successfully, and produced a value.
+struct SuccessWithValue: ExecutionResultSuccess {
+    var description: String
+}
+
+/// There was an error preprocessing the code.
+struct PreprocessorError: ExecutionResultError {
+    var exception: PreprocessorException
+    
+    var description: String {
+        exception.errorDescription
+    }
+}
+
+struct PreprocessorException: LocalizedError {
+    var errorDescription: String?
+    init(_ message: String) { errorDescription = message }
+}
+
+struct PackageInstallException: LocalizedError {
+    var errorDescription: String?
+    init(_ message: String) { errorDescription = message }
+}
+
+/// There was a compile or runtime error.
+struct SwiftError: ExecutionResultError {
+    var description: String
+}
