ccac291da9b64459154ab2f5482dab4cc087ea27
Philip Turner <71743241+philipturner@users.noreply.github.com>
2022-03-10 19:32:30 -0500

    Update PreprocessAndExecute.swift

--- Sources/JupyterKernel/SwiftKernel/PreprocessAndExecute.swift
+++ Sources/JupyterKernel/SwiftKernel/PreprocessAndExecute.swift
@@ -4,4 +4,22 @@ fileprivate func preprocessLine(index lineIndex: Int, line: String) throws -> St
   return line
 }
 
-fileprivate func execute(code: String) 
+// TODO: test that this function works
+fileprivate func execute(code: String) throws -> ExecutionResult {
+  var descriptionPtr: UnsafeMutablePointer<CChar>?
+  let err = KernelContext.execute(code, &descriptionPtr)
+  
+  var description: String?
+  if let descriptionPtr = descriptionPtr {
+    description = String(cString: descriptionPtr)
+    free(descriptionPtr)
+  }
+  
+  if err == 0 {
+    return SuccessWithValue(description: description!)
+  } else if err == 1 {
+    return SuccessWithoutValue()
+  } else {
+    return SwiftError(description: description!)
+  }
+}
