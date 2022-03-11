22b234cc79d53e774eddf819a2d03246e0d9460f
Philip Turner <71743241+philipturner@users.noreply.github.com>
2022-03-11 16:30:43 -0500

    Update lldb_process.cpp

--- Sources/lldb-process/lldb_process.cpp
+++ Sources/lldb-process/lldb_process.cpp
@@ -98,8 +98,7 @@ int execute(const char *code, char **description) {
 }
 
 int get_stdout(char *dst, int *buffer_size) {
-  process.GetSTDOUT(dst, size_t(buffer_size));
-  return 0;
+  return int(process.GetSTDOUT(dst, size_t(buffer_size)));
 }
   
 
