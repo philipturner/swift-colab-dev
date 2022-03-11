e5874e81e7fa2a4a8bac8eaf94de65c13e33aa6a
Philip Turner <71743241+philipturner@users.noreply.github.com>
2022-03-10 20:52:57 -0500

    Update IOHandlers.swift

--- Sources/JupyterKernel/SwiftKernel/IOHandlers.swift
+++ Sources/JupyterKernel/SwiftKernel/IOHandlers.swift
@@ -6,7 +6,9 @@ let SIGINTHandler = PythonClass(
   "SIGINTHandler",
   superclasses: [threading.Thread],
   members: [
-    
+    "__init__": PythonInstanceMethod { (`self`: PythonObject) in
+      self.daemon = true
+    }              
   ]
 ).pythonObject
 
