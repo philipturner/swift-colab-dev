4e7168b00bfc348fc66070b6bde567ad7de1542e
Philip Turner <71743241+philipturner@users.noreply.github.com>
2022-03-13 16:14:22 -0400

    Update ExecutionResult.swift

--- Sources/JupyterKernel/SwiftKernel/ExecutionResult.swift
+++ Sources/JupyterKernel/SwiftKernel/ExecutionResult.swift
@@ -1,51 +1,51 @@
 import Foundation
 
 /// Protocol for the result of executing code.
-protocol ExecutionResult: CustomStringConvertible { }
+protocol ExecutionResult: CustomStringConvertible { 
+  var description: String { get }
+}
 
 /// Protocol for the result of successfully executing code.
-protocol ExecutionResultSuccess: ExecutionResult { }
+protocol ExecutionResultSuccess: ExecutionResult {}
 
 /// Protocol for the result of unsuccessfully executing code.
-protocol ExecutionResultError: ExecutionResult { 
-    var description: String { get }
-}
+protocol ExecutionResultError: ExecutionResult {}
 
 /// The code executed successfully, and did not produce a value.
 struct SuccessWithoutValue: ExecutionResultSuccess {
-    var description: String { "" }
+  var description: String { "" }
 }
 
 /// The code executed successfully, and produced a value.
 struct SuccessWithValue: ExecutionResultSuccess {
-    var description: String
+  var description: String
 }
 
 /// There was an error preprocessing the code.
 struct PreprocessorError: ExecutionResultError {
-    var exception: PreprocessorException
-    
-    var description: String {
-        String(describing: exception)
-    }
+  var exception: PreprocessorException
+
+  var description: String {
+    String(describing: exception)
+  }
 }
 
 /// There was a compile or runtime error.
 struct SwiftError: ExecutionResultError {
-    var description: String
+  var description: String
 }
 
 struct Exception: LocalizedError {
-    var errorDescription: String?
-    init(_ message: String) { errorDescription = message }
+  var errorDescription: String?
+  init(_ message: String) { errorDescription = message }
 }
 
 struct PreprocessorException: LocalizedError {
-    var errorDescription: String?
-    init(_ message: String) { errorDescription = message }
+  var errorDescription: String?
+  init(_ message: String) { errorDescription = message }
 }
 
 struct PackageInstallException: LocalizedError {
-    var errorDescription: String?
-    init(_ message: String) { errorDescription = message }
+  var errorDescription: String?
+  init(_ message: String) { errorDescription = message }
 }
