b737f34a0d90e4c748fc41207df37976265b5967
Philip Turner <71743241+philipturner@users.noreply.github.com>
2022-03-14 12:09:25 -0400

    Update lldb_process.cpp

--- Sources/lldb-process/lldb_process.cpp
+++ Sources/lldb-process/lldb_process.cpp
@@ -205,7 +205,11 @@ int after_successful_execution(uint64_t **serialized_output) {
     
     for (uint32_t j = 0; j < num_byte_arrays; ++j) {
       auto byte_array = display_message.GetChildAtIndex(j);
-      read_byte_array(byte_array, &output_size, &output_capacity, &output);
+      error_code = read_byte_array(
+        byte_array, &output_size, &output_capacity, &output);
+      if (error_code != 0) {
+        return 1 + error_code;
+      }
     }
   }
   
