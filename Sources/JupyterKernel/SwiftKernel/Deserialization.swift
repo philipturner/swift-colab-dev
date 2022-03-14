d45022c5150b471bce3ade448cd364c7487d143f
Philip Turner <71743241+philipturner@users.noreply.github.com>
2022-03-14 12:24:57 -0400

    Update Deserialization.swift

--- Sources/JupyterKernel/SwiftKernel/Deserialization.swift
+++ Sources/JupyterKernel/SwiftKernel/Deserialization.swift
@@ -1,14 +1,17 @@
 import Foundation
 
-func afterSuccessfulExecution() {
+func afterSuccessfulExecution() throws {
   var serializedOutput: UnsafeMutablePointer<UInt64>?
   let error = KernelContext.after_successful_execution(&serializedOutput)
-  if error != 0 {
-    print("C++ part of `afterSuccessfulExecution` failed with error code \(error)")
-    precondition(serializedOutput == nil)
-  } else {
-    print("C++ part of `afterSuccessfulExecution` succeeded.")
-    precondition(serializedOutput != nil)
-    free(serializedOutput)
+  guard let serializedOutput = serializedOutput else {
+    throw Exception(
+      "C++ part of `afterSuccessfulExecution` failed with error code \(error).")
   }
+   
+  let output = deserialize(executionOutput: serializedOutput)
+  free(serializedOutput)
+}
+
+fileprivate func deserialize(executionOutput: UnsafeMutablePointer<UInt64>) -> [[String]] {
+  return []
 }
