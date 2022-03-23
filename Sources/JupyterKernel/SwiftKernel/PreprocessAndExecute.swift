28a0c16257b8338a73eae45d5847635dd8a92878
Philip Turner <71743241+philipturner@users.noreply.github.com>
2022-03-23 17:13:27 -0400

    Update PreprocessAndExecute.swift

--- Sources/JupyterKernel/SwiftKernel/PreprocessAndExecute.swift
+++ Sources/JupyterKernel/SwiftKernel/PreprocessAndExecute.swift
@@ -27,7 +27,7 @@ func execute(code: String, lineIndex: Int? = nil) -> ExecutionResult {
   var description: String?
   if let descriptionPtr = descriptionPtr {
     description = String(cString: descriptionPtr)
-    print("Swift length stats: \(strlen(descriptionPtr)), \(description.count)")
+    print("Swift length stats: \(strlen(descriptionPtr)), \(description?.count)")
     print("On Swift side: \(description)")
     free(descriptionPtr)
   }
