6b4b7003f0ead8a69af808eda8a5a77014840845
Philip Turner <71743241+philipturner@users.noreply.github.com>
2022-03-26 16:19:25 -0400

    Update ProcessInstalls.swift

--- Sources/JupyterKernel/SwiftKernel/ProcessInstalls.swift
+++ Sources/JupyterKernel/SwiftKernel/ProcessInstalls.swift
@@ -34,7 +34,7 @@ func processInstallDirective(
   if isValidDirective { return }
   
   try attempt(command: processInstall, ###"""
-    ^\s*%install-location (.*)$
+    ^\s*%install (.*)$
     """###)
   if isValidDirective { return }
 }
