5c62516d9b26d5225cf525eb4708121d52a59eb1
Philip Turner <71743241+philipturner@users.noreply.github.com>
2022-03-26 12:40:35 -0400

    Update ProcessInstalls.swift

--- Sources/JupyterKernel/SwiftKernel/ProcessInstalls.swift
+++ Sources/JupyterKernel/SwiftKernel/ProcessInstalls.swift
@@ -5,7 +5,7 @@ fileprivate let shlex = Python.import("shlex")
 func processInstallDirective(
   line: String, isValidDirective: inout Bool
 ) throws {
-  func attempt(regex: String, command: (String) throws -> Void) rethrows {
+  func attempt(_ regex: String, command: (String) throws -> Void) rethrows {
     let regexMatch = re.match(regex, line)
     if regexMatch != Python.None {
       let restOfLine = String(regexMatch.group(1))!
@@ -14,8 +14,7 @@ func processInstallDirective(
     }
   }
   
-  try attempt(
-    regex: ###"""
+  try attempt(###"""
     ^\s*%install-swiftpm-flags (.*)$
     """###, 
     command: processSwiftPMFlags)
