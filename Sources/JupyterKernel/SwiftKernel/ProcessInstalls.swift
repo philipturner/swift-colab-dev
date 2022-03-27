36054ad05a9e74640258852af14a2d31855c3de2
Philip Turner <71743241+philipturner@users.noreply.github.com>
2022-03-27 15:06:24 -0400

    Update ProcessInstalls.swift

--- Sources/JupyterKernel/SwiftKernel/ProcessInstalls.swift
+++ Sources/JupyterKernel/SwiftKernel/ProcessInstalls.swift
@@ -325,20 +325,26 @@ fileprivate func processInstall(
                                       stdout: subprocess.PIPE,
                                       stderr: subprocess.STDOUT,
                                       cwd: packagePath)
+  
   // Whenever the Swift package has been built at least one time before, it
-  // outputs a massive, ugly JSON blob that cannot be suppressed. This workaround
-  // filters it out. There is little chance SwiftPM will respond with arbitrary
-  // strings that accidentally trigger this erasure.
+  // outputs a massive, ugly JSON blob that cannot be suppressed. This
+  // workaround filters that out.
   var currentlyInsideBrackets = false
+  
   for buildOutputLine in Python.iter(
       buildProcess.stdout.readline, PythonBytes(Data())) {
     let str = String(buildOutputLine.decode("utf8"))!
     guard str.hasSuffix("\n") else {
-      throw Exception("A build output line from SwiftPM did not end with \"\n\".")
-================================================================================
+      throw Exception("""
+        A build output line from SwiftPM did not end with "\n":
+        \(str)
+        """)
+    }
+// =============================================================================
+// TODO: implement the mechanism
 //     if Bool(Python.isinstance(buildOutputLine, Python.string))! {
 //       sendStdout(, insertNewLine: false) // try "true"
-      sendStdout(String(describing: buildOutputLine), insertNewLine: true)
+      sendStdout(String(describing: buildOutputLine), insertNewLine: false)
 //     }
   }
   let buildReturnCode = buildProcess.wait()
