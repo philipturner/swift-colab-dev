5e4b5794d163fa6803ea08ec7b2520209cf4885c
Philip Turner <71743241+philipturner@users.noreply.github.com>
2022-03-26 20:03:44 -0400

    Update ProcessInstalls.swift

--- Sources/JupyterKernel/SwiftKernel/ProcessInstalls.swift
+++ Sources/JupyterKernel/SwiftKernel/ProcessInstalls.swift
@@ -253,7 +253,7 @@ fileprivate func processInstall(
   let packageNameQuoted = "\"\(packageName)\""
   
   // Contents of the Swift package manifest
-  let /*communist*/ manifest /*o*/ = // :-)
+  let /*communist*/ manifest /*o*/ = // ☭
   """
   // swift-tools-version:4.2
   import PackageDescription
