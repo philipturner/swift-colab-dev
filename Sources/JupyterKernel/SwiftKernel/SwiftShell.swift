45bdbf7067a4b5183d78b50ec73e79807c72daad
Philip Turner <71743241+philipturner@users.noreply.github.com>
2022-03-30 18:17:56 -0400

    Update SwiftShell.swift

--- Sources/JupyterKernel/SwiftKernel/SwiftShell.swift
+++ Sources/JupyterKernel/SwiftKernel/SwiftShell.swift
@@ -2,3 +2,13 @@ import Foundation
 fileprivate let eventloops = Python.import("ipykernel.eventloops")
 fileprivate let session = Python.import("jupyter_client.session")
 fileprivate let zmqshell = Python.import("ipykernel.zmqshell")
+================================================================================
+// Simulates a ZMQ socket, saving messages instead of sending them. We use this 
+// to capture display messages.
+fileprivate let CapturingSocket = PythonClass(
+  "CapturingSocket",
+  superclasses: [session.Session],
+  members: [
+    
+  ]
+).pythonObject
