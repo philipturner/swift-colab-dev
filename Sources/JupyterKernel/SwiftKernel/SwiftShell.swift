9a77cfee68487aecfe6f9b95a0599945f14be5ed
Philip Turner <71743241+philipturner@users.noreply.github.com>
2022-03-30 18:15:00 -0400

    Update SwiftShell.swift

--- Sources/JupyterKernel/SwiftKernel/SwiftShell.swift
+++ Sources/JupyterKernel/SwiftKernel/SwiftShell.swift
@@ -1,2 +1,4 @@
 import Foundation
 fileprivate let eventloops = Python.import("ipykernel.eventloops")
+fileprivate let session = Python.import("jupyter_client.session")
+fileprivate let zmqshell = Python.import("ipykernel.zmqshell")
