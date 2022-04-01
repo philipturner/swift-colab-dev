bcfe982c5f16d00ef965a1864513504a4c17c542
Philip Turner <71743241+philipturner@users.noreply.github.com>
2022-04-01 16:46:55 -0400

    Update SwiftShell.swift

--- Sources/JupyterKernel/SwiftShell.swift
+++ Sources/JupyterKernel/SwiftShell.swift
@@ -21,7 +21,7 @@ public func create_shell(
   
   let username = String(cString: username_ptr)
   let sessionID = String(cString: sessionID_ptr)
-  let key = PythonBytes(String(cString: key_ptr).pythonObject)
+  let key = PythonBytes(String(cString: key_ptr).pythonObject)!
   
   let socket = CapturingSocket()
   let session = Session(username: username, session: sessionID, key: key)
