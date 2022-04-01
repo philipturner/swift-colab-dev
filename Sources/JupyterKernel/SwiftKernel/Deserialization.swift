d035119bdea308f61a1b7c2403e8056c79f0e000
Philip Turner <71743241+philipturner@users.noreply.github.com>
2022-04-01 18:40:29 -0400

    Update Deserialization.swift

--- Sources/JupyterKernel/SwiftKernel/Deserialization.swift
+++ Sources/JupyterKernel/SwiftKernel/Deserialization.swift
@@ -7,38 +7,30 @@ func afterSuccessfulExecution() throws {
     throw Exception(
       "C++ part of `afterSuccessfulExecution` failed with error code \(error).")
   }
-   
+  
   let output = try deserialize(executionOutput: serializedOutput)
   print("KernelCommunicator produced: \(output)")
   free(serializedOutput)
 }
 
-fileprivate func deserialize(executionOutput: UnsafeMutablePointer<UInt64>) throws -> [[String]] {
+fileprivate func deserialize(executionOutput: UnsafeMutablePointer<UInt64>) throws -> [String] {
   var stream = executionOutput
-  let numJupyterMessages = Int(stream.pointee)
+  let numParts = Int(stream.pointee)
   stream += 1
   
-  var jupyterMessages: [[String]] = []
-  jupyterMessages.reserveCapacity(numJupyterMessages)
-  for _ in 0..<numJupyterMessages {
-    let numDisplayMessages = Int(stream.pointee)
+  var parts: [String] = []
+  parts.reserveCapacity(numJupyterMessages)
+  for _ in 0..<numParts {
+    let numBytes = Int(stream.pointee)
     stream += 1
-    
-    var displayMessages: [String] = []
-    displayMessages.reserveCapacity(numDisplayMessages)
-    for _ in 0..<numDisplayMessages {
-      let numBytes = Int(stream.pointee)
-      stream += 1
-      
-      let byteArray = Data(
-        bytesNoCopy: stream, count: numBytes, deallocator: .none)
-      guard let message = String(data: byteArray, encoding: .utf8) else {
-        throw Exception("Could not decode bytes: \(byteArray.map { $0 })")
-      }
-      displayMessages.append(message)
-      stream += (numBytes + 7) / 8
+
+    let byteArray = Data(
+      bytesNoCopy: stream, count: numBytes, deallocator: .none)
+    guard let message = String(data: byteArray, encoding: .utf8) else {
+      throw Exception("Could not decode bytes: \(byteArray.map { $0 })")
     }
-    jupyterMessages.append(displayMessages)
+    parts.append(message)
+    stream += (numBytes + 7) / 8
   }
   
   return jupyterMessages
